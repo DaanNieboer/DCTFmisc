4803407027ecbdf1efe4edcba8631756966c7008
Daan Nieboer <daan.nieboer@gmail.com>
2018-02-15 21:46:58 +0100

    created utility functions

---
+++ R/utility_functions.R
@@ -0,0 +1,10 @@
+not_na_count <- function(x) sum(!is.na(x))
+
+filter_groups_by_size <- function(x, grp, min_size = 3){
+  nr_points <- aggregate(x, by = list(grp), FUN = not_na_count)
+  grp_sel   <- nr_points$Group.1[nr_points$x>=min_size]
+
+  res <- data.frame(value = x, group = grp)
+  res <- res[res$group %in% grp_sel, ]
+  return(res)
+}
